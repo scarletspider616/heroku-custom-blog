8fdd62b69b35c3504a4f9524a9199f7643b8ccd4
Joey Fallone <j.fallone@ualberta.ca>
2022-02-08 23:06:12 -0700

    Remove secret key field
    (note this key was never/will never be used in prod so there is no concern with it remaining in git history)

--- proj/settings.py
+++ proj/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-twmfb_+@p7(q$-*#+(^s^lc2^+j91f0_d4nor$99hw$mzwecfn'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
